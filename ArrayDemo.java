2aa29029cfb044fb36d2fbabeaf60fc8ea7941e1
CodyBagwandeen <cbagwandeen@stuy.edu>
2018-09-27 16:59:57 -0400

    0a done

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,14 +1,21 @@
 public class ArrayDemo{
 
-  int[] anArray = { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+  static int[] anArray = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+
   public static void printArray(int[]ary){
     String s = "";
     for( int x = 0; x < ary.length; x++) {
       s += ary[x];
+      if ( x < ary.length -1)
+      s += ", ";
     }
-    System.out.print(s);
+    System.out.print( "[" + s + "]");
   }
 
 
-System.out.println(printArray( anArray))
+
+
+public static void main(String[] args) {
+  printArray(anArray);
+}
 }
