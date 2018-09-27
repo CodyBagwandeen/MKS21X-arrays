50f015c8be20ace6898fd674e7196a88704056ce
CodyBagwandeen <cbagwandeen@stuy.edu>
2018-09-27 17:29:26 -0400

    0b done

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -2,6 +2,8 @@ public class ArrayDemo{
 
   static int[] anArray = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
 
+  static int[][] multiC = new int[][]{{0,0,0,0},{0,0,0,0},{0,0,0,0},};
+
   public static void printArray(int[]ary){
     String s = "";
     for( int x = 0; x < ary.length; x++) {
@@ -12,10 +14,31 @@ public class ArrayDemo{
     System.out.print( "[" + s + "]");
   }
 
+  public static void printArray(int[][]ary){
+    String s = "";
+    for ( int r = 0; r < ary.length ; r++) {
+      for ( int c = 0; c < ary[r].length; c++ ) {
+        s += ary[r][c];
+        if ( c < ary[r].length -1)
+        s += ", ";
+        if ( c == ary[r].length -1)
+        s+= "\n";
+      }
+    }
+    System.out.print("[" + s + "]");
+  }
+
+
+
+
+
+
 
 
 
 public static void main(String[] args) {
   printArray(anArray);
+  System.out.print(" \n");
+  printArray(multiC);
 }
 }
